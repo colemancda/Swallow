048e52a73f37c4d8832def59db160078b86b2283
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-18 23:41:37 -0500

    created Xcode project

---
+++ Xcode/Swallow/Swallow.h
@@ -0,0 +1,19 @@
+//
+//  Swallow.h
+//  Swallow
+//
+//  Created by Alsey Coleman Miller on 1/18/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swallow.
+FOUNDATION_EXPORT double SwallowVersionNumber;
+
+//! Project version string for Swallow.
+FOUNDATION_EXPORT const unsigned char SwallowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swallow/PublicHeader.h>
+
+
