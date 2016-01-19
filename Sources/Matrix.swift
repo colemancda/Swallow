4df569065faebedaa3ea2cdaf9d05d7dcb7face6
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-19 12:51:01 -0500

    added Matrix type

---
+++ Sources/Matrix.swift
@@ -0,0 +1,16 @@
+//
+//  Matrix.swift
+//  Swallow
+//
+//  Created by Alsey Coleman Miller on 1/19/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+/// Describes an affine, 2D transformation Matrix. 
+/// It provides methods to manipulate the matrix in convenient ways, and can be used to transform points.
+public struct Matrix {
+    
+    public var a, b, c, d, tx, ty: Float
+    
+    
+}
