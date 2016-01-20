6da4d5f8eca533b1d66551b24a6cd12091bf173f
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-20 01:30:25 -0500

    implemented VertexColor

--- Sources/Swallow/VertexColor.swift
+++ Sources/Swallow/VertexColor.swift
@@ -18,6 +18,11 @@ public struct VertexColor {
     
     public var alpha: UInt8
     
+    public var isOpaqueWhite: Bool {
+        
+        return alpha == 255 && red == 255 && green == 255 && blue == 255
+    }
+    
     // MARK: - Initialization
     
     public init(red: UInt8 = 0, green: UInt8 = 0, blue: UInt8 = 0, alpha: UInt8 = 0) {
@@ -30,10 +35,36 @@ public struct VertexColor {
     
     public init(color: Color, alpha: Float) {
         
+        assert(alpha <= 1.0 && alpha >= 0, "Invalid alpha value: \(alpha)")
+        
         self.red = color.red
         self.green = color.green
         self.blue = color.blue
         self.alpha = UInt8(alpha * Float(225.0))
     }
     
+    // MARK: - Methods
+    
+    public mutating func premultiplyAlpha() {
+        
+        let alpha = Float(self.alpha / 255)
+        
+        guard alpha != 1.0 else { return }
+        
+        self.red    = UInt8(Float(red)   * alpha)
+        self.green  = UInt8(Float(green) * alpha)
+        self.blue   = UInt8(Float(blue)  * alpha)
+    }
+    
+    public mutating func unmultiplyAlpha() {
+        
+        let alpha = Float(self.alpha / 255)
+        
+        guard (alpha == 0.0 || alpha == 1.0) == false else { return }
+        
+        self.red    = UInt8(Float(red)   / alpha)
+        self.green  = UInt8(Float(green) / alpha)
+        self.blue   = UInt8(Float(blue)  / alpha)
+    }
 }
+
