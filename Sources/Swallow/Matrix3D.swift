5c23d3609b601b26af1c007399d86d7726900992
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-21 01:56:38 -0500

    added Matrix3D, Point3D

---
+++ Sources/Swallow/Matrix3D.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix3D.swift
+//  Swallow
+//
+//  Created by Alsey Coleman Miller on 1/21/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+/// Represents a transformation matrix that determines the position and orientation
+/// of a three-dimensional (3D) display object. The matrix can perform transformation functions
+/// including translation (repositioning along the x, y, and z axes), rotation, and scaling (resizing).
+public struct Matrix3D {
+    
+    public var matrix: Matrix4
+    
+    
+}
---
+++ Sources/Swallow/Vector4.swift
@@ -0,0 +1,13 @@
+//
+//  Vector4.swift
+//  Swallow
+//
+//  Created by Alsey Coleman Miller on 1/21/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+
+public struct Vector4 {
+    
+    
+}
