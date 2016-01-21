b27c6c718360ae807e31767cec5d82c638ffea1e
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-20 20:16:23 -0500

    added Matrix3

---
+++ Sources/Swallow/Matrix3.swift
@@ -0,0 +1,12 @@
+//
+//  Matrix3.swift
+//  Swallow
+//
+//  Created by Alsey Coleman Miller on 1/20/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+public struct Matrix3 {
+    
+    
+}
